6f907c5870608d28b66962c4be9f57e25f17ca1e
Your Name <you@example.com>
2023-05-22 13:25:01 +0530

    ..

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/mern11002023?retryWrites=true&w=majority'
+const url = 'mongodb+srv://akhilyadav21262:1234@cluster0.e7l9gaq.mongodb.net/smartx?retryWrites=true&w=majority'
 
 mongoose.connect(url)
 .then((result) => {
